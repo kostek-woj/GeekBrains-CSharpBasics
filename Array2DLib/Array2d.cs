c81d5040e6839487423a09706114927d0c3b886c
Kostek Woj <kostek.woj88@gmail.com>
2021-06-07 15:16:33 +0300

    Lesson005: Homework Complete

---
+++ Array2DLib/Array2d.cs
@@ -0,0 +1,199 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Array2DLib
+{
+    public class Array2d
+    {
+        private int[,] _arr;
+
+        public Array2d() {
+
+        }
+        public Array2d(int dimension, int val) {
+            _arr = new int[dimension, dimension];
+            for (int i = 0; i < dimension; i++) {
+                for (int j = 0; j < dimension; j++) {
+                    _arr[i, j] = val;
+                }
+            }
+        }
+
+        public Array2d(int dimension, int min, int max) {
+            _arr = new int[dimension, dimension];
+            Random rnd = new Random();
+            for (int i = 0; i < dimension; i++) {
+                for (int j = 0; j < dimension; j++) {
+                    _arr[i, j] = rnd.Next(min, max);
+                }
+            }
+        }
+
+        public Array2d(string fileName) {
+
+            string fullFileName = @"../../" + fileName;
+            string[] arrFromFile = { };
+
+            try {
+                arrFromFile = File.ReadAllLines(fullFileName);
+            } catch (FileNotFoundException e) {
+                Console.WriteLine($"\nFile {fileName} is not found:\n{e.Message}");
+            } catch (IOException e) {
+                Console.WriteLine($"\nSomething went wrong:\n{e.Message}");
+            }
+
+            _arr = new int[arrFromFile.Length, arrFromFile.Length];
+
+            for (int i = 0; i < arrFromFile.Length; i++) {
+                string[] row = arrFromFile[i].Split(' ');
+                for (int j = 0; j < arrFromFile.Length; j++) {
+                    _arr[i, j] = Convert.ToInt32(row[j]);
+                }
+            }
+        }
+
+        public int Min {
+            get {
+                int min = _arr[0, 0];
+                for (int i = 0; i < _arr.GetLength(0); i++) {
+                    for (int j = 0; j < _arr.GetLength(1); j++) {
+                        if (_arr[i, j] < min) {
+                            min = _arr[i, j];
+                        }
+                    }
+                }
+                return min;
+            }
+        }
+
+        public int Max {
+            get {
+                int max = _arr[0, 0];
+                for (int i = 0; i < _arr.GetLength(0); i++) {
+                    for (int j = 0; j < _arr.GetLength(1); j++) {
+                        if (_arr[i, j] > max) {
+                            max = _arr[i, j];
+                        }
+                    }
+                }
+                return max;
+            }
+        }
+        
+        public int CountPositive {
+            get {
+                int count = 0;
+                for (int i = 0; i < _arr.GetLength(0); i++) {
+                    for (int j = 0; j < _arr.GetLength(1); j++) {
+                        if (_arr[i, j] > 0) {
+                            count++;
+                        }
+                    }
+                }
+                return count;
+            }
+        }
+        
+        public double Average {
+            get {
+                double sum = 0;
+                for (int i = 0; i < _arr.GetLength(0); i++) {
+                    for (int j = 0; j < _arr.GetLength(1); j++) {
+                        sum += _arr[i, j];
+                    }
+                }
+                return sum / (_arr.GetLength(0) * _arr.GetLength(1));
+            }
+        }
+
+        public int Sum() {
+            int sum = 0;
+            for (int i = 0; i < _arr.GetLength(0); i++) {
+                for (int j = 0; j < _arr.GetLength(1); j++) {
+                    sum += _arr[i, j];
+                }
+            }
+            return sum;
+        }
+
+        public int SumOfGreaterThan(int number) {
+            int sum = 0;
+            for (int i = 0; i < _arr.GetLength(0); i++) {
+                for (int j = 0; j < _arr.GetLength(1); j++) {
+                    if (_arr[i, j] > number) {
+                        sum += _arr[i, j];
+                    }
+                }
+            }
+            return sum;
+        }
+
+        public void IndexOfBiggestElement(out int[] index) {
+            index = new int[] { -1, -1 };
+            int max = this._arr[0, 0];
+            for (int i = 0; i < this._arr.GetLength(0); i++) {
+                for (int j = 0; j < this._arr.GetLength(1); j++) {
+                    if (this._arr[i, j] > max) {
+                        max = this._arr[i, j];
+                        index[0] = i;
+                        index[1] = j;
+                    }
+                }
+            }
+        }
+
+        public void readFromFile(string fileName) {
+            string fullFileName = @"../../" + fileName;
+            string[] arrFromFile = { };
+
+            try {
+                arrFromFile = File.ReadAllLines(fullFileName);
+            } catch (FileNotFoundException e) {
+                Console.WriteLine($"\nFile {fileName} is not found:\n{e.Message}");
+            } catch (IOException e) {
+                Console.WriteLine($"\nSomething went wrong:\n{e.Message}");
+            }
+
+            _arr = new int[arrFromFile.Length, arrFromFile.Length];
+
+            for (int i = 0; i < arrFromFile.Length; i++) {
+                string[] row = arrFromFile[i].Split(' ');
+                for (int j = 0; j < arrFromFile.Length; j++) {
+                    _arr[i, j] = Convert.ToInt32(row[j]);
+                }
+            }
+        }
+
+        public void writeToFile(string fileName) {
+            string fullFileName = @"../../" + fileName;
+            try {
+                StreamWriter sw = new StreamWriter(fullFileName);
+                for (int i = 0; i < _arr.GetLength(0); i++) {
+                    for (int j = 0; j < _arr.GetLength(1); j++) {
+                        sw.Write(_arr[i, j] + 
+                            ((j < _arr.GetLength(1) - 1) ? " " : ""));
+                    }
+                    sw.WriteLine();
+                }
+                sw.Close();
+            } catch (IOException e) {
+                Console.WriteLine($"\nSomething went wrong:\n{e.Message}");
+            }
+        }
+
+        public override string ToString() {
+            string s = "";
+            for (int i = 0; i < _arr.GetLength(0); i++) {
+                for (int j = 0; j < _arr.GetLength(1); j++) {
+                    s += _arr[i, j] + "\t";
+                }
+                s += "\n";
+            }
+            return s;
+        }
+    }
+}
---
+++ Message/Message.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Message
+{
+    public static class Message
+    {
+        public static void PrintWordsShorterThanN(string msg, int n) {
+            
+            StringBuilder msgSb = new StringBuilder(); // for string without punctuation and numbers
+            char[] msgArr = msg.ToCharArray();
+            
+            for (int i = 0; i < msgArr.Length; i++) {
+                if (!Char.IsDigit(msgArr[i]) && !Char.IsPunctuation(msgArr[i])) {
+                    msgSb.Append(msgArr[i]);
+                }
+            }
+
+            string[] msgNew = msgSb.ToString().Split(' ');
+
+            Console.WriteLine($"\nAll the words equal or shorter than {n}:");
+
+            for (int i = 0; i < msgNew.GetLength(0); i++) {
+                if (msgNew[i].Length <= n && msgNew[i].Length != 0) {
+                    Console.WriteLine(msgNew[i].Length + "\t" + msgNew[i]);
+                }
+            }
+        }
+
+
+        public static void RemoveWordsEndingIn(string msg, char c) {
+            
+            string[] msgArr = msg.Trim().Split(' ');
+            StringBuilder sb = new StringBuilder();
+            
+            foreach (string s in msgArr) {
+                if (s[s.Length - 1] != c) {
+                    sb.Append(s + " ");
+                }
+            }
+            
+            Console.WriteLine(sb.ToString().Trim());
+        }
+
+
+        public static string LongestWord(string msg) {
+            
+            string[] msgArr = msg.Trim().Split(' ');
+            string longestWord = msgArr[0];
+            
+            foreach (string s in msgArr) {
+                if (s.Length > longestWord.Length) {
+                    longestWord = s;
+                }
+            }
+            
+            return longestWord;
+        }
+
+
+        public static void LongestWordsString(string msg) {
+            
+            int longestLength = LongestWord(msg).Length;
+            StringBuilder sb = new StringBuilder();
+            string[] msgArr = msg.Trim().Split(' ');
+            
+            foreach (string s in msgArr) {
+                if (s.Length == longestLength) {
+                    sb.Append(s + " ");
+                }
+            }
+
+            Console.WriteLine($"\nLongest words make the following sentence:\n{sb.ToString().Trim()}");
+        }
+
+
+        public static Dictionary<string, int> FrequencyTextAnalysis(string msg) {
+            
+            string[] msgArr = msg.Trim().Split(' ');
+            Dictionary<string, int> frequency = new Dictionary<string, int>();
+
+            foreach (string s in msgArr) {
+                if (!frequency.ContainsKey(s)) {
+                    frequency.Add(s, 1);
+                } else {
+                    frequency[s]++;
+                }
+            }
+
+            foreach (KeyValuePair<string, int> kvp in frequency.OrderByDescending(key => key.Value).ThenBy(key => key.Key)) {
+                Console.WriteLine($"{kvp.Value}\t{kvp.Key}");
+            }
+
+            return frequency; // return value since it was demanded to do so in the task :)
+        }
+    }
+}
