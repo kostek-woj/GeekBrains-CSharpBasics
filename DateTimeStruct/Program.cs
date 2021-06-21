8f6cb5d6ddb78244185d7c4418327d638f493d5f
Kostek Woj <kostek.woj88@gmail.com>
2021-06-21 15:36:25 +0300

    Lesson 008 - Homework Tasks 1 & 2 done

---
+++ DateTimeStruct/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Reflection;
+
+namespace DateTimeStruct
+{
+    class Program
+    {
+        static PropertyInfo GetPropertyInfo(object obj, string str) {
+            return obj.GetType().GetProperty(str);
+        }
+
+        static void Main(string[] args) {
+            Console.WriteLine("All properties of DateTime Struct:");
+
+            DateTime dt = new DateTime();
+
+            Console.WriteLine($"CanRead: { GetPropertyInfo(dt, "Year").CanRead }");
+            Console.WriteLine($"CanWrite: { GetPropertyInfo(dt, "Year").CanWrite }");
+            Console.WriteLine($"Attributes: { GetPropertyInfo(dt, "Year").Attributes }");
+            Console.WriteLine($"CustomAttributes: { GetPropertyInfo(dt, "Year").CustomAttributes }");
+            Console.WriteLine($"DeclaringType: { GetPropertyInfo(dt, "Year").DeclaringType }");
+            Console.WriteLine($"GetMethod: { GetPropertyInfo(dt, "Year").GetMethod }");
+            Console.WriteLine($"IsSpecialName: { GetPropertyInfo(dt, "Year").IsSpecialName }");
+            Console.WriteLine($"MemberType: { GetPropertyInfo(dt, "Year").MemberType }");
+            Console.WriteLine($"MetadataToken: { GetPropertyInfo(dt, "Year").MetadataToken }");
+            Console.WriteLine($"Module: { GetPropertyInfo(dt, "Year").Module }");
+            Console.WriteLine($"Name: { GetPropertyInfo(dt, "Year").Name }");
+            Console.WriteLine($"PropertyType: { GetPropertyInfo(dt, "Year").PropertyType }");
+            Console.WriteLine($"ReflectedType: { GetPropertyInfo(dt, "Year").ReflectedType }");
+            Console.WriteLine($"SetMethod: { GetPropertyInfo(dt, "Year").SetMethod }");
+            Console.WriteLine($"GetValue: { GetPropertyInfo(dt, "Year").GetValue(dt) }");
+        }
+    }
+}
---
+++ UpsAndDowns/Form1.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace UpsAndDowns
+{
+    public partial class Form1 : Form
+    {
+        public Form1() {
+            InitializeComponent();
+            textBox1.Text = numericUpDown1.Value.ToString();
+        }
+
+        private void numericUpDown1_ValueChanged(object sender, EventArgs e) {
+            textBox1.Text = numericUpDown1.Value.ToString();
+        }
+    }
+}
